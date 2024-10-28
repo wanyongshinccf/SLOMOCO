5b0cf543713b3a7dc9eecadfd1a658c4183d3ba4
Wanyong Shin <shinw@ccf.org>
2024-10-27 21:19:55 -0500

    initialized

--- calc_iFD.py
+++ calc_iFD.py
@@ -9,119 +9,80 @@ in_arr = sys.argv
 if '-sli' not in in_arr :
     print (__doc__)
     raise NameError('error: -sli option is not provided')
-elif '-zdim' not in in_arr :
+elif '-tdim' not in in_arr :
     print (__doc__)
-    raise NameError('error: -zdim option is not provided')
+    raise NameError('error: -tdim option is not provided')
 else:
     slireg1D = in_arr[in_arr.index('-sli') + 1]
-    zdim     = in_arr[in_arr.index('-zdim') + 1]
+    tdim     = in_arr[in_arr.index('-tdim') + 1]
    
 
 # read 1D files [ tdim x (zdim * 6 mopa)]
 slireg = np.loadtxt(slireg1D)
 #slireg = np.loadtxt('slimot_py_fit.txt')
 
-# define tdim, vardim (=zdim x 6)
+# define zmbdim, slireg = [ zmbdim*tdim x 6]
 dims = np.shape(slireg)
-tdim = dims[0]/int(zdim)
+zmbdim = dims[0]/int(tdim)
+# zmbdim = zdim (slice number) / MB acceleration
 
-# dx,dy,dz,rx,ry,rz
-dx = slireg[:,4]
-dy = slireg[:,5]
-dz = slireg[:,3]
-rx = slireg[:,1]
-ry = slireg[:,2]
-rz = slireg[:,0]
-
-# radius (mm)
-R1 = 50 
+# define variables; radius (mm)
+R1 = 50
+R2 = 80 
 d2r = 0.01745329
 
+# dx,dy,dz,rx,ry,rz
+dx = -1 * slireg[:,4]
+dy = -1 * slireg[:,5]
+dz = -1 * slireg[:,3]
+rx = d2r* slireg[:,1]
+ry = d2r* slireg[:,2]
+rz = d2r* slireg[:,0]
+
 # calculate iFD (Power)
-FDP_sli = np.abs(dx) + np.abs(dy) + np.abs(dz) + R1*d2r*(np.abs(rx)+np.abs(ry)+np.abs(rz));
-temp = np.reshape(FDP_sli,(int(tdim),int(zdim)))
+FDP_sli = np.abs(dx) + np.abs(dy) + np.abs(dz) + R1*(np.abs(rx)+np.abs(ry)+np.abs(rz));
+temp = np.reshape(FDP_sli,(int(tdim),int(zmbdim)))
 iFDP = np.mean(temp, axis=1)
 
 # calculate ioFD (Power)
-FDP_sli = np.abs(dz) + R1*d2r*(np.abs(rx)+np.abs(ry));
-temp = np.reshape(FDP_sli,(int(tdim),int(zdim)))
+FDP_sli = np.abs(dz) + R1*(np.abs(rx)+np.abs(ry));
+temp = np.reshape(FDP_sli,(int(tdim),int(zmbdim)))
 ioFDP = np.mean(temp, axis=1)
 
-# calculate iFD (Jankinson)
-R2 = 80
-d2r = 0.01745329
-iFDJ_sli = np.zeros((int(zdim)*int(tdim),1))
-for iz in range(0, int(zdim)*int(tdim)-1): 
-     iddx = dx[iz]
-     iddy = dy[iz]
-     iddz = dz[iz]
-     idrx = rx[iz]
-     idry = ry[iz]
-     idrz = rz[iz]
-     
-     # rotation matrix calclation
-     xrotmat = np.array([[1, 0, 0], [0, cal.cos(idrx*d2r), -1*cal.sin(idrx*d2r)],	[0, cal.sin(idrx*d2r), cal.cos(idrx*d2r)]])
-     yrotmat = np.array([[cal.cos(idry*d2r), 0, cal.sin(idry*d2r)], [0, 1, 0], [-1*cal.sin(idry*d2r), 0, cal.cos(idry*d2r)]])
-     zrotmat = np.array([[cal.cos(idrz*d2r), -1*cal.sin(idrz*d2r), 0], [cal.sin(idrz*d2r), cal.cos(idrz*d2r), 0], [0, 0, 1]])
-     xyzrotmat = np.dot(zrotmat,np.dot(yrotmat, xrotmat))
-     M = np.trace(np.dot(np.transpose(xyzrotmat-np.eye(3)),xyzrotmat-np.eye(3)))
-     sumdxyz = iddx*iddx + iddy*iddy + iddz*iddz
-     iFDJ_sli[iz,0] = np.sqrt(R2 * R2 / 5.0 * M + sumdxyz )
+
+# calculate iFD and izFD (Jenkinson)
+iFDJ  = np.zeros((int(tdim),1))
+izFDJ = np.zeros((int(tdim),1))
+for t in range (0, int(tdim)):
+    ifdj = 0
+    izfdj = 0
+    for iz in range(0, int(zmbdim)): 
+        # read volumetric params first (assume AFNI 3dvolreg)
+        # 3dvolreg motion parametner
+        # z-rot, x-rot, y-rot, z-shift, x-shift, y-shift 
+        # 3dallineate motion parameter 
+        # -xshift, -y-shift, -z-shift, z-rot, x-rot, y-rot
+        dx = -1 * slireg[iz+t*int(zmbdim),4]
+        dy = -1 * slireg[iz+t*int(zmbdim),5]
+        dz = -1 * slireg[iz+t*int(zmbdim),3]
+        rx = d2r* slireg[iz+t*int(zmbdim),1]
+        ry = d2r* slireg[iz+t*int(zmbdim),2]
+        rz = d2r* slireg[iz+t*int(zmbdim),0]
+        # rotation matrix calclation
+        xrotmat = np.array([[1, 0, 0], [0, cal.cos(rx), -1*cal.sin(rx)], [0, cal.sin(rx), cal.cos(rx)]])
+        yrotmat = np.array([[cal.cos(ry), 0, cal.sin(ry)], [0, 1, 0], [-1*cal.sin(ry), 0, cal.cos(ry)]])
+        zrotmat = np.array([[cal.cos(rz), -1*cal.sin(rz), 0], [cal.sin(rz), cal.cos(rz), 0], [0, 0, 1]])
+        xyzrotmat = np.dot(zrotmat,np.dot(yrotmat, xrotmat))
+        M = np.dot(np.transpose(xyzrotmat-np.eye(3)),xyzrotmat-np.eye(3))
+        ifdj  = ifdj  + np.sqrt(R2 * R2 / 5.0 * (M[0,0] + M[1,1] + M[2,2]) + dx*dx + dy*dy + dz*dz)
+        izfdj = izfdj + np.sqrt(R2 * R2 / 5.0 * M[2,2] + dz*dz)
+    iFDJ[t,0] =  ifdj/zmbdim
+    izFDJ[t,0] = izfdj/zmbdim
 
 
-# reshape; FDJ_sli is [tdim*zdim,1] vectort
-temp = np.reshape(iFDJ_sli,(int(tdim),int(zdim)))
-iFDJ = np.mean(temp, axis=1)
-
-# ioFD
-iFDJ_sli = np.zeros((int(zdim)*int(tdim),1))
-for iz in range(0, int(zdim)*int(tdim)-1): 
-     iddx = 0
-     iddy = 0
-     iddz = dz[iz]
-     idrx = rx[iz]
-     idry = ry[iz]
-     idrz = 0
-     
-     # rotation matrix calclation
-     xrotmat = np.array([[1, 0, 0], [0, cal.cos(idrx*d2r), -1*cal.sin(idrx*d2r)],	[0, cal.sin(idrx*d2r), cal.cos(idrx*d2r)]])
-     yrotmat = np.array([[cal.cos(idry*d2r), 0, cal.sin(idry*d2r)], [0, 1, 0], [-1*cal.sin(idry*d2r), 0, cal.cos(idry*d2r)]])
-     zrotmat = np.array([[cal.cos(idrz*d2r), -1*cal.sin(idrz*d2r), 0], [cal.sin(idrz*d2r), cal.cos(idrz*d2r), 0], [0, 0, 1]])
-     xyzrotmat = np.dot(zrotmat,np.dot(yrotmat, xrotmat))
-     M = np.trace(np.dot(np.transpose(xyzrotmat-np.eye(3)),xyzrotmat-np.eye(3)))
-     sumdxyz = iddx*iddx + iddy*iddy + iddz*iddz
-     iFDJ_sli[iz,0] = np.sqrt(R2 * R2 / 5.0 * M + sumdxyz )
-
-
-# reshape; FDJ_sli is [tdim*zdim,1] vectort
-temp = np.reshape(iFDJ_sli,(int(tdim),int(zdim)))
-ioFDJ = np.mean(temp, axis=1)
-
-izFDJ_sli = np.zeros((int(zdim)*int(tdim),1))
-for iz in range(0, int(zdim)*int(tdim)-1): 
-     iddx = dx[iz]
-     iddy = dy[iz]
-     iddz = dz[iz]
-     idrx = rx[iz]
-     idry = ry[iz]
-     idrz = rz[iz]
-     
-     # rotation matrix calclation
-     xrotmat = np.array([[1, 0, 0], [0, cal.cos(idrx*d2r), -1*cal.sin(idrx*d2r)],	[0, cal.sin(idrx*d2r), cal.cos(idrx*d2r)]])
-     yrotmat = np.array([[cal.cos(idry*d2r), 0, cal.sin(idry*d2r)], [0, 1, 0], [-1*cal.sin(idry*d2r), 0, cal.cos(idry*d2r)]])
-     zrotmat = np.array([[cal.cos(idrz*d2r), -1*cal.sin(idrz*d2r), 0], [cal.sin(idrz*d2r), cal.cos(idrz*d2r), 0], [0, 0, 1]])
-     xyzrotmat = np.dot(zrotmat,np.dot(yrotmat, xrotmat))
-
-     M = np.dot(np.transpose(xyzrotmat-np.eye(3)),xyzrotmat-np.eye(3))
-     izFDJ_sli[iz,0] = np.sqrt(R2 * R2 / 5.0 * M[2,2] + iddz*iddz )
-
-# reshape; FDJ_sli is [tdim*zdim,1] vectort
-temp = np.reshape(izFDJ_sli,(int(tdim),int(zdim)))
-izFDJ = np.mean(temp, axis=1)
 
 # write the result
 np.savetxt('iFDJ_py.txt',iFDJ)	
 np.savetxt('iFDP_py.txt',iFDP)
-np.savetxt('ioFDJ_py.txt',ioFDJ)	
-np.savetxt('ioFDP_py.txt',ioFDP)
+np.savetxt('ioFDP_py.txt',ioFDP)	
 np.savetxt('izFDJ_py.txt',izFDJ)
